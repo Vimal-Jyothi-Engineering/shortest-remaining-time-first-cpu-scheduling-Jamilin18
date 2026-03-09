aa345fd8612378824295b150640f78d450ace222
JAMILIN GEORGE <jamilingeorge@gmail.com>
2026-03-09 14:19:21 +0530

    This C program implements the Shortest Remaining Time First (SRTF) CPU scheduling algorithm. It takes the arrival time and burst time of processes as input and selects the process with the smallest remaining burst time for execution at each time unit. If a new process arrives with a shorter remaining time, the CPU switches to that process. The program then calculates and displays the waiting time, turnaround time, and their averages for all processes.

--- srtf.c
+++ srtf.c
@@ -1,33 +1,66 @@
 #include <stdio.h>
 
-struct process {
-    char pid[10];
-    int at, bt, rt;
-    int ct, wt, tat;
-};
-
 int main() {
-    int n;
+    int n, i, time = 0, smallest;
+    int completed = 0;
+    int arrival[20], burst[20], remaining[20];
+    int waiting[20], turnaround[20];
+    int finish_time;
+    int total_wt = 0, total_tat = 0;
+
+    printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    struct process p[20];
+    for(i = 0; i < n; i++) {
+        printf("\nProcess %d\n", i+1);
 
-    for(int i = 0; i < n; i++) {
-        scanf("%s %d %d", p[i].pid, &p[i].at, &p[i].bt);
-        p[i].rt = p[i].bt;
+        printf("Arrival Time: ");
+        scanf("%d", &arrival[i]);
+
+        printf("Burst Time: ");
+        scanf("%d", &burst[i]);
+
+        remaining[i] = burst[i];
     }
 
-    ..
-      .
-      ..
-      .
-      ..
-      .
-      ..
-      
+    remaining[19] = 9999; // sentinel value
 
-    printf("Average Waiting Time: %.1f\n", avg_wt/n);
-    printf("Average Turnaround Time: %.1f\n", avg_tat/n);
+    while(completed != n) {
+
+        smallest = 19;
+
+        for(i = 0; i < n; i++) {
+            if(arrival[i] <= time && remaining[i] < remaining[smallest] && remaining[i] > 0) {
+                smallest = i;
+            }
+        }
+
+        remaining[smallest]--;
+
+        if(remaining[smallest] == 0) {
+            completed++;
+
+            finish_time = time + 1;
+
+            waiting[smallest] = finish_time - burst[smallest] - arrival[smallest];
+            turnaround[smallest] = finish_time - arrival[smallest];
+
+            total_wt += waiting[smallest];
+            total_tat += turnaround[smallest];
+        }
+
+        time++;
+    }
+
+    printf("\n\nProcess\tArrival\tBurst\tWaiting\tTurnaround\n");
+
+    for(i = 0; i < n; i++) {
+        printf("P%d\t%d\t%d\t%d\t%d\n",
+               i+1, arrival[i], burst[i], waiting[i], turnaround[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", (float)total_wt/n);
+    printf("\nAverage Turnaround Time = %.2f\n", (float)total_tat/n);
 
     return 0;
 }
